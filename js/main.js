ef7f105607dbfacefb2910cee837cc132c06e8e1
KALININ ALEXEY <2257855@gmail.com>
2021-04-09 02:27:13 +0300

    1

--- js/main.js
+++ js/main.js
@@ -3,14 +3,30 @@ $(document).ready(function () {
     // Optional parameters
     direction: "horizontal",
     loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      },
+    pagination: ".pag-shoes",
+    paginationClickable: true,
+    paginationType: "custom",
+    paginationCustomRender: function (swiper, current, total) {
+      var names = [];
+      $(".swiper-wrapper .swiper-slide").each(function (i) {
+        names.push($(this).data("name"));
+      });
+      var text = "<span style='background-color:black;padding:20px;'>";
+      for (let i = 1; i <= total; i++) {
+        if (current == i) {
+          text +=
+            "<span style='border-top:1px solid green;margin-right:4px;color:green;padding:10px;'>" +
+            names[i] +
+            "</span>";
+        } else {
+          text +=
+            "<span style='border-top:1px solid white;margin-right:4px;color:white;padding:10px;'>" +
+            names[i] +
+            "</span>";
+        }
+      }
+      text += "</span>";
+      return text;
     },
   });
 });
